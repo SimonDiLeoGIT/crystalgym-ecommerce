02afe86dfebdc4135706e47ad36a68cc2a0b2d10
SimonDiLeoGIT <saimondiley@gmail.com>
2024-01-31 16:38:31 -0300

    Add New  Posts

--- ecomerce-template/src/components/post_section/News_Posts_Section.tsx
+++ ecomerce-template/src/components/post_section/News_Posts_Section.tsx
@@ -3,7 +3,7 @@ interface Post {
   image: string,
   title: string,
   info: string,
-  price: DoubleRange,
+  price: number,
 }
 
 type PostsList = Post[]
@@ -13,12 +13,21 @@ interface Props {
 }
 
 export const News_Posts_Section: React.FC<Props> = ({ posts }) => {
+
+
   return (
-    <section>
+    <section className="mt-24 ml-4 flex overflow-x-auto whitespace-nowrap">
       {posts.map(post => {
         return (
-          <article key={post.id}>
-            {post.title}
+          <article className="min-w-72 mx-1" key={post.id}>
+            <figure className="mb-4 h-96">
+              <img className="w-full h-5/6 object-cover" src={post.image} alt={post.title} />
+              <figcaption className="mt-2">
+                <h1 className="text-lg font-bold">{post.title}</h1>
+                <p className="text-sm">{post.info}</p>
+                <p className="text-sm">${post.price}</p>
+              </figcaption>
+            </figure>
           </article>
         )
       })}
--- ecomerce-template/src/pages/Home.tsx
+++ ecomerce-template/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { News_Posts_Section } from "../components/post_section/News_Posts_Section"
 import home_header_img from "./../assets/Home_images/home_header_img.png"
+import posts from "../assets/posts.json"
 
 export const Home = () => {
   return (
-    <section className="">
+    <section className="max-w-screen overflow-x-hidden">
       <header>
         <img src={home_header_img} />
         <h1 className="absolute top-2/4 left-4 mt-20 font-bold text-xl">NEW <span className="-text--color-dark-violet">THIS</span> MONTH</h1>
@@ -12,7 +14,7 @@ export const Home = () => {
           <button className="font-roboto -bg--color-black -text--color-light-grey-violet font-bold p-4 rounded-full w-5/6 my-2">NEW PRODUCTS</button>
         </section>
       </header>
-
+      <News_Posts_Section posts={posts.posts} />
     </section>
   )
 }
