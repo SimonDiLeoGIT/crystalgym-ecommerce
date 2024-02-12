faee8a005f5f21804f146e51db1d52d50db3116c
SimonDiLeoGIT <saimondiley@gmail.com>
2024-02-12 15:24:33 -0300

    add generic carousel

--- clothing-ecommerce/src/components/Carousel.tsx
+++ clothing-ecommerce/src/components/Carousel.tsx
@@ -2,9 +2,7 @@ import { Link } from 'react-router-dom'
 import '../styles/global/carousel.css'
 import { useState } from 'react'
 
-interface Advertisement {
-  id: string,
-  image: string,
+interface image {
   title_front: string,
   title_middle: string,
   title_end: string,
@@ -13,46 +11,76 @@ interface Advertisement {
   button_link: string,
 }
 
-type AdvertisementsType = Advertisement[]
+interface Image {
+  id: string,
+  image: string,
+}
+
+type ImagesType = Image[]
 
 interface Props {
-  advertisements: AdvertisementsType
+  images: ImagesType,
+  advertisement: image
 }
 
 
-export const Carousel: React.FC<Props> = ({ advertisements }) => {
+export const Carousel: React.FC<Props> = ({ advertisement, images }) => {
 
-  const [currentAdvertisement, changeCurrentAdvertisement] = useState(0);
+  const [currentimage, changeCurrentimage] = useState(0);
+  const [translateValue, setTranslateValue] = useState(0);
 
-  function nextAdvertisement() {
-    if (currentAdvertisement < 2) {
-      changeCurrentAdvertisement(currentAdvertisement + 1);
+  function nextimage() {
+    let newIndex = (currentimage + 1);
+    if (currentimage < 2) {
+      changeCurrentimage(currentimage + 1);
     } else {
-      changeCurrentAdvertisement(0);
+      changeCurrentimage(0);
+      newIndex = 0;
     }
+    setTranslateValue(-100 * newIndex);
+  }
+
+  function previmage() {
+    let newIndex = (currentimage - 1);
+    if (currentimage > 0) {
+      changeCurrentimage(currentimage - 1);
+    } else {
+      changeCurrentimage(2);
+      newIndex = 2;
+    }
+    setTranslateValue(-100 * newIndex);
   }
 
   return (
-    <section className='relative w-screen h-screen'>
-      <section
-        className='w-screen h-screen bg-cover bg-center grid place-content-end gap-12 carousel-animation'
-        style={{ backgroundImage: `url(${advertisements[currentAdvertisement].image})` }}
-      >
-        <section className='w-screen px-5'>
-          <h1 className="font-bold text-2xl">{advertisements[currentAdvertisement].title_front}<span className="-text--color-light-grey-violet -bg--color-black rounded-md px-2 mx-2">{advertisements[currentAdvertisement].title_middle}</span> {advertisements[currentAdvertisement].title_end}</h1>
-          <h2 className="font-semibold text-sm">{advertisements[currentAdvertisement].info}</h2>
+    <section className='relative overflow-hidden w-screen h-screen'>
+      <section className='flex transition-transform duration-500 ease-in-out' style={{ transform: `translateX(${translateValue}%)` }}>
+        {images.map((image) => {
+          return (
+            <img
+              src={image.image}
+              alt={image.id}
+              className='w-screen h-screen object-cover'
+            />
+          )
+        })}
+      </section>
+      <section className='absolute top-2/3 grid gap-12'>
+        <section className=' w-screen p-5 -bg--color-very-light-grey'>
+          <h1 className="font-bold text-2xl">{advertisement.title_front}<span className="-text--color-light-grey-violet -bg--color-black rounded-md px-2 mx-2">{advertisement.title_middle}</span> {advertisement.title_end}</h1>
+          <p className="font-semibold text-sm">{advertisement.info}</p>
         </section>
-        <Link to={advertisements[currentAdvertisement].button_link} className="text-center -bg--color-light-grey-violet font-bold p-4 rounded-full w-11/12 m-auto mb-4">{advertisements[currentAdvertisement].button_title}</Link>
+        <Link to={advertisement.button_link} className="text-center -bg--color-light-grey-violet font-bold p-4 rounded-full w-11/12 m-auto mb-4">{advertisement.button_title}</Link>
       </section>
       <div className='absolute w-screen grid grid-cols-2 top-1/2'>
         <button
           className='m-auto ml-4'
+          onClick={() => previmage()}
         >
           A
         </button>
         <button
           className='m-auto mr-4'
-          onClick={() => nextAdvertisement()}
+          onClick={() => nextimage()}
         >
           A
         </button>
--- clothing-ecommerce/src/pages/Home.tsx
+++ clothing-ecommerce/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Posts_Section } from "../components/post_section/Posts_Section"
 import posts from "../assets/json/posts.json"
-import advertisements from "../assets/json/home/advertisements.json"
+import new_this_month from "../assets/json/home/advertisement/new-this-month.json"
+import off_20 from "../assets/json/home/advertisement/20-off.json"
 import './../styles/news.css'
 import { Carousel } from "../components/Carousel"
 
@@ -9,10 +10,13 @@ export const Home = () => {
     <main className="max-w-screen overflow-x-hidden font-roboto">
       <header>
         <Carousel
-          advertisements={advertisements}
+          advertisement={new_this_month.advertisement} images={new_this_month.images}
         />
       </header>
       <Posts_Section posts={posts.posts} title="New This Month" />
+      <Carousel
+        advertisement={off_20.advertisement} images={off_20.images}
+      />
       <Posts_Section posts={posts.posts} title="20% Off" />
     </main>
   )
