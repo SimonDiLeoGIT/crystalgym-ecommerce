4f2894f7991754a57b30e9d29cf6a81061239c77
SimonDiLeoGIT <saimondiley@gmail.com>
2024-03-05 12:44:51 -0300

    Comienza Footer Responsive

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,10 +5,10 @@ import git from "../../assets/icons/social_media_icons/github.svg"
 
 export const Footer = () => {
   return (
-    <footer className="-bg--color-black -text--color-white px-4 mt-4">
-      <nav className="text-center p-2">
-        <ul>
-          <li className="p-2">
+    <footer className="-bg--color-black -text--color-white px-4 mt-4 lg:grid lg:grid-cols-2">
+      <nav className="text-center p-2 m-auto max-w-lg lg:text-start">
+        <ul className="">
+          <li className="p-2 ">
             <Link to="/">Home</Link>
           </li>
           <li className="p-2">
@@ -19,7 +19,7 @@ export const Footer = () => {
           </li>
         </ul>
       </nav>
-      <section className="">
+      <section className="m-auto max-w-lg lg:row-span-2 lg:m- lg:p-8">
         <header>
           <h1 className="text-lg font-medium">Sign Up for Email</h1>
           <p className="my-2">
@@ -27,7 +27,7 @@ export const Footer = () => {
             exclusive content, events and more!
           </p>
         </header>
-        <form className="">
+        <form className="m-auto max-w-lg">
           <input
             type="text" placeholder="Name"
             className="w-full p-2 -bg--color-very-light-grey -text--color-black placeholder:-text--color-black"
@@ -42,11 +42,11 @@ export const Footer = () => {
           />
         </form>
       </section>
-      <section className="">
+      <section className="m-auto max-w-lg lg:m-auto">
         <header>
           <h1>Contact Us</h1>
         </header>
-        <ul className="flex place-content-center my-4">
+        <ul className="flex place-content-center my-4 lg:place-content-start">
           <li>
             <a href="https://www.linkedin.com/in/simondileodev/" target="_blank">
               <img
@@ -78,7 +78,7 @@ export const Footer = () => {
         <p className="text-sm">e-commerce@gmail.com</p>
         <p className="text-sm mt-2">Gowland, Buenos Aires 513 n° 223</p>
       </section>
-      <p className="py-4 w-full text-center">&copy; 2024 e-commerce</p>
+      <p className="py-4 w-full text-center lg:col-span-2">&copy; 2024 e-commerce</p>
     </footer>
   )
 }
