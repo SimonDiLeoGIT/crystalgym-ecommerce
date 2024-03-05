f84b5674fcb6f1441545a4418b29fd4782e26fdd
SimonDiLeoGIT <saimondiley@gmail.com>
2024-03-05 11:04:42 -0300

    fix: Relaciona botones de carousel al scrollbar

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -53,45 +53,45 @@ export const Carousel: React.FC<Props> = ({ advertisement, images }) => {
   }
 
   return (
-    <section className='relative overflow-hidden w-screen h-screen'>
-      <section className='flex transition-transform duration-500 ease-in-out' style={{ transform: `translateX(${translateValue}%)` }}>
-        {images.map((image) => {
-          return (
-            <img
-              src={image.image}
-              alt={image.id}
-              className='w-screen h-screen object-cover'
-            />
-          )
-        })}
-      </section>
-      <section className='absolute top-2/3 grid gap-12'>
-        <section className=' w-screen p-5 -bg--color-very-light-grey bg-opacity-60'>
+    <section className='relative w-full overflow-x-auto'>
+      <div className='overflow-hidden'>
+        <section className='flex transition-transform duration-500 ease-in-out' style={{ transform: `translateX(${translateValue}%)` }}>
+          {images.map((image) => {
+            return (
+              <img
+                src={image.image}
+                alt={image.id}
+                className='w-screen h-screen object-cover md:h-1/2'
+              />
+            )
+          })}
+        </section>
+      </div>
+      <section className='absolute top-2/3 grid gap-12 w-full'>
+        <section className=' w-full p-5 -bg--color-very-light-grey bg-opacity-60'>
           <h1 className="font-bold text-2xl">{advertisement.title_front}<span className="-text--color-light-grey-violet -bg--color-black rounded-md px-2 mx-2">{advertisement.title_middle}</span> {advertisement.title_end}</h1>
           <p className="font-semibold text-sm">{advertisement.info}</p>
         </section>
-        <Link to={advertisement.button_link} className="text-center -bg--color-light-grey-violet font-bold p-4 rounded-full w-11/12 m-auto mb-4">{advertisement.button_title}</Link>
+        <Link to={advertisement.button_link} className="text-center -bg--color-light-grey-violet font-bold p-4 rounded-full w-11/12 max-w-lg m-auto mb-4">{advertisement.button_title}</Link>
       </section>
-      <div className='absolute w-screen grid grid-cols-2 top-1/2'>
-        <button
-          className='m-auto ml-4 rounded-full -bg--color-light-grey-violet opacity-60 shadow-md -shadow--color-greyest-violet'
-          onClick={() => previmage()}
-        >
-          <img
-            src={left_arrow} alt=""
-            width='30px'
-          />
-        </button>
-        <button
-          className='m-auto mr-4 rounded-full -bg--color-light-grey-violet opacity-60 shadow-md -shadow--color-greyest-violet'
-          onClick={() => nextimage()}
-        >
-          <img
-            src={right_arrow} alt=""
-            width='30px'
-          />
-        </button>
-      </div>
+      <button
+        className='absolute top-1/2 left-4 rounded-full -bg--color-light-grey-violet opacity-60 shadow-md -shadow--color-greyest-violet'
+        onClick={() => previmage()}
+      >
+        <img
+          src={left_arrow} alt=""
+          width='30px'
+        />
+      </button>
+      <button
+        className='absolute top-1/2 right-4 rounded-full -bg--color-light-grey-violet opacity-60 shadow-md -shadow--color-greyest-violet'
+        onClick={() => nextimage()}
+      >
+        <img
+          src={right_arrow} alt=""
+          width='30px'
+        />
+      </button>
     </section>
   )
 }
