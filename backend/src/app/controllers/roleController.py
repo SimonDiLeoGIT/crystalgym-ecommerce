3036a9fa658ea24d969531518c3675766afd6fcb
Simón Di Leo <127776583+SimonDiLeoGIT@users.noreply.github.com>
2024-08-24 10:00:42 -0300

    Delete backend directory

--- backend/src/app/controllers/roleController.py
+++
@@ -1,5 +0,0 @@
-from flask import jsonify, Blueprint
-# import services
-from app.services.roleService import RoleService
-# import utils
-from app.utils.errorResponseHandler import ErrorResponseHandler
--- backend/src/app/services/roleService.py
+++
@@ -1,5 +0,0 @@
-from app.repositories.roleRepository import RoleRepository
-
-class RoleService:
-    def __init__(self):
-        self.role_repository = RoleRepository()
